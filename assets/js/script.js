11e6c2ed4e26c4ed23f3a0a4adef940de46a9b6c
Alphaam <58554818+Alphaam@users.noreply.github.com>
2020-12-14 03:54:01 +0400

    Portfolio
    Portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    $("#heading").fadeOut(3000);
+    $("#About-Image").fadeOut(3000);
+    $("#button1").fadeOut(3000);
+    $("#heading").fadeIn(3000);
+     $("#image1").fadeIn(3000);
+    $("#image2").fadeIn(3000);
+    $("#image3").fadeIn(3000);
+    $("#image4").fadeIn(3000);
+    $("#howTo").fadeIn(3000);
+    $("#socialMedia").fadeIn(3000);
+    $("#button2").fadeIn(3000);
+  });
+});
+
+$(document).ready(function(){
+  $("#button2").click(function(){
+    
+    $("#About-Image").fadeIn(4000);
+    $("#button1").fadeIn(4000);
+    
+     $("#image1").fadeOut(3000);
+    $("#image2").fadeOut(3000);
+    $("#image3").fadeOut(3000);
+    $("#image4").fadeOut(3000);
+    $("#howTo").fadeOut(3000);
+    $("#socialMedia").fadeOut(3000);
+    $("#button2").fadeOut(3000);
+  });
+});
